e82690fd9416eb535a354f12095cf28b354e0aae
Harsharamesh06 <119047967+Harsharamesh06@users.noreply.github.com>
2025-02-14 15:57:56 +0530

    Create stack.java

---
+++ stack.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+Stack<String> stack = new Stacko();
+stack. push ("Maip()");
+stack. push("Print ()"):
+stack. push("PrintHello()");
+        
+for(String str : stack){
+System.out.printla(str);
+}
+        while(!stack.isEmpty())"{
+            System.out.println(stack.pop());
+    }
+    for(String str : stack){
+        System.out.println(str);
+    }
+        }
+    }
+}
